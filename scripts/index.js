e32bcff46da498d919b96c03b15d575ee3f7c608
Vladimir Galkin <galkin.ve.home@gmail.com>
2023-11-10 10:52:14 +0300

    Add creat, delete and initial card functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const cardsListElement = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+/**
+ * Функция создания карточки
+ * @param {object} cardInfo информация о карточке
+ * @param {function} deleteCard функция удаления карточки
+ * @returns {object} карточка
+ */
+function createCard(cardInfo, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector('.card__image').src = cardInfo.link;
+  cardElement.querySelector('.card__title').textContent = cardInfo.name;
 
-// @todo: Вывести карточки на страницу
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
+
+/**
+ * Функция удаления карточки
+ * @param {object} element карточка для удаления
+ */
+function deleteCard(element) {
+  element.remove();
+}
+
+// Вывод карточки на страницу
+initialCards.forEach((card) => {
+  const temp = createCard(card, deleteCard)
+  cardsListElement.append(temp);
+});
