9963b7e88204350f1c485eaaac3cb2fdcb2cf20c
Vladimir Galkin <galkin.ve.home@gmail.com>
2023-11-29 16:55:06 +0300

    Hotfix - change js module to default export

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import './../pages/index.css'
-import { initialCards } from './cards.js'
+import initialCards from './cards.js'
 
 // Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
